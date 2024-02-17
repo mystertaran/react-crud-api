dcf3279b047dc0d62333e51ebe9afd3926e32aeb
Szymon Taranczewski <sz.taranczewski@gmail.com>
2024-02-17 12:55:52 +0100

    implementing Users on database for future processing

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, 'Phone number is required'],
+    },
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
